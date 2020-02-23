9880754e1353c9c01cb4a05145672242f82fc543
xjapanseikima <magican213@gmail.com>
2020-02-22 22:31:21 -0800

    Fixed login page

--- app/src/main/java/com/example/rideshare_00/GetList/getList.java
+++ app/src/main/java/com/example/rideshare_00/GetList/getList.java
@@ -5,29 +5,29 @@ import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 
-import com.example.rideshare_00.PostList.postTrip;
 import androidx.appcompat.app.AppCompatActivity;
-
+import com.example.rideshare_00.PostList.postTrip;
 import com.example.rideshare_00.R;
 
 public class getList extends AppCompatActivity {
 
-    //Button create_btn= findViewById(R.id.create_btn);
+   //
     @Override
     protected void onCreate(Bundle savedInstanceState ){
-        super.onCreate(savedInstanceState);
         setContentView(R.layout.get_list);
-//        Intent intent = new Intent(this, postTrip.class);
-//        startActivity(intent);
-//        create_btn.setOnClickListener(new Button.OnClickListener(){
-//            @Override
-//            public void onClick(View v) {
-//                Intent intent = new Intent(this, postTrip.class);
-//                startActivity(intent);
-//            }
-//        });
+        super.onCreate(savedInstanceState);
+        Button create_btn= findViewById(R.id.create_btn);
+        create_btn.setOnClickListener(new Button.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(getList.this, postTrip.class);
+                startActivity(intent);
+            }
+        });
     }
+    public void PostTrip(){
 
-
+    }
 
 }
--- app/src/main/java/com/example/rideshare_00/PostList/postTrip.java
+++ app/src/main/java/com/example/rideshare_00/PostList/postTrip.java
@@ -8,7 +8,7 @@ import com.example.rideshare_00.R;
 
 public class postTrip extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState ){
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.post_trip);
     }
