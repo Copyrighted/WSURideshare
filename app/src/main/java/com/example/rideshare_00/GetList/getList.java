b41e251f41bc1988d594fec95a26a7a1d8a93ae2
xjapanseikima <magican213@gmail.com>
2020-02-22 22:10:02 -0800

    Fixed login page

--- app/src/main/java/com/example/rideshare_00/GetList/getList.java
+++ app/src/main/java/com/example/rideshare_00/GetList/getList.java
@@ -12,13 +12,13 @@ import com.example.rideshare_00.R;
 
 public class getList extends AppCompatActivity {
 
-    Button create_btn= findViewById(R.id.create_btn);
+    //Button create_btn= findViewById(R.id.create_btn);
     @Override
     protected void onCreate(Bundle savedInstanceState ){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get_list);
-        Intent intent = new Intent(this, postTrip.class);
-        startActivity(intent);
+//        Intent intent = new Intent(this, postTrip.class);
+//        startActivity(intent);
 //        create_btn.setOnClickListener(new Button.OnClickListener(){
 //            @Override
 //            public void onClick(View v) {
