38cc513f556d74b1f79ade930143bb6b7400a256
Mariana <m.rodriguezrojas@wsu.edu>
2020-02-23 08:25:51 -0800

    PLEASE WORK PART!2!!!!

---
+++ app/src/main/java/com/example/rideshare_00/ui/login/activityRegister.java
@@ -0,0 +1,42 @@
+package com.example.rideshare_00.ui.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toolbar;
+
+import com.example.rideshare_00.R;
+
+public class activityRegister extends AppCompatActivity {
+
+    private Button button;
+    private Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        //setSupportActionBar(toolbar);
+
+        button = (Button) findViewById(R.id.registerBack);
+        button.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                goBacktoMeanActivity();
+            }
+
+        });
+
+    }
+    //function to go back to Main View after registering
+    public void goBacktoMeanActivity(){
+        Intent intent = new Intent (this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+}
