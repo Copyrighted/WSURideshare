b25a0a2ac8a37a2f841adb309cd319579d603445
angel <angelxd84130@gmail.com>
2020-02-22 23:35:25 -0800

    show the post list

--- app/src/main/java/com/example/rideshare_00/GetList/getList.java
+++ app/src/main/java/com/example/rideshare_00/GetList/getList.java
@@ -2,19 +2,62 @@ package com.example.rideshare_00.GetList;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
+import android.widget.ListView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.rideshare_00.PostList.postTrip;
 import com.example.rideshare_00.R;
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class getList extends AppCompatActivity {
 
-   //
+
     @Override
     protected void onCreate(Bundle savedInstanceState ){
         setContentView(R.layout.get_list);
+        ListView list = findViewById(R.id.show_list);
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
+        list.setAdapter(adapter);
+        final FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference contacts = database.getReference("give");
+        contacts.addChildEventListener(new ChildEventListener() {
+            @Override
+            public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+                adapter.add((String)dataSnapshot.child("from").getValue()+ "->"+ (String)dataSnapshot.child("to").getValue());
+
+
+            }
+
+            @Override
+            public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
+                adapter.remove((String) dataSnapshot.child("from").getValue()+ "->" +(String) dataSnapshot.child("to").getValue());
+
+            }
+
+            @Override
+            public void onChildMoved(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
         super.onCreate(savedInstanceState);
         Button create_btn= findViewById(R.id.create_btn);
         create_btn.setOnClickListener(new Button.OnClickListener(){
