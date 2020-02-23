edae318f0cd7540a6e4ac26de13b2aaff2b1ab8b
Mariana <m.rodriguezrojas@wsu.edu>
2020-02-23 03:47:32 -0800

    :(

--- app/src/main/java/com/example/rideshare_00/ui/login/LoginActivity.java
+++ app/src/main/java/com/example/rideshare_00/ui/login/LoginActivity.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 import android.os.Bundle;
+import android.content.Intent;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,24 +19,36 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+
 import com.example.rideshare_00.R;
-import com.example.rideshare_00.ui.login.LoginViewModel;
-import com.example.rideshare_00.ui.login.LoginViewModelFactory;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LoginViewModel loginViewModel;
+    private Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button = (Button) findViewById(R.id.registerBtn);
+        button.setOnClickListener( new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                openRegisterActivity();
+            }
+
+        });
+
+
+
         loginViewModel = ViewModelProviders.of(this, new LoginViewModelFactory())
                 .get(LoginViewModel.class);
 
         final EditText usernameEditText = findViewById(R.id.username);
         final EditText passwordEditText = findViewById(R.id.password);
-        final Button loginButton = findViewById(R.id.login);
+        final Button loginButton = findViewById(R.id.signinBtn);
         final ProgressBar loadingProgressBar = findViewById(R.id.loading);
 
         loginViewModel.getLoginFormState().observe(this, new Observer<LoginFormState>() {
@@ -104,15 +117,6 @@ public class LoginActivity extends AppCompatActivity {
                 return false;
             }
         });
-
-        loginButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                loadingProgressBar.setVisibility(View.VISIBLE);
-                loginViewModel.login(usernameEditText.getText().toString(),
-                        passwordEditText.getText().toString());
-            }
-        });
     }
 
     private void updateUiWithUser(LoggedInUserView model) {
@@ -123,5 +127,10 @@ public class LoginActivity extends AppCompatActivity {
 
     private void showLoginFailed(@StringRes Integer errorString) {
         Toast.makeText(getApplicationContext(), errorString, Toast.LENGTH_SHORT).show();
+    }
+    //function to open Register Activity view
+    public void openRegisterActivity(){
+        Intent intent = new Intent (this, registerActivity.class);
+        startActivity(intent);
     }
 }
