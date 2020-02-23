0f19ac62601f1bca03b730e1f8b51ee87d0b0634
xjapanseikima <magican213@gmail.com>
2020-02-23 07:55:08 -0800

    Fixed login page

--- app/src/main/java/com/example/rideshare_00/GetList/getList.java
+++ app/src/main/java/com/example/rideshare_00/GetList/getList.java
@@ -1,7 +1,9 @@
 package com.example.rideshare_00.GetList;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.view.View;
@@ -9,6 +11,7 @@ import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import com.example.rideshare_00.PostList.postTrip;
 import com.example.rideshare_00.R;
@@ -19,10 +22,15 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import android.os.Handler;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+
 public class getList extends AppCompatActivity {
 
     DatabaseReference contacts;
     FirebaseDatabase database;
+    ArrayList<String> a_list;
     int i=0;
     @Override
     protected void onCreate(Bundle savedInstanceState ){
@@ -30,6 +38,7 @@ public class getList extends AppCompatActivity {
         database = FirebaseDatabase.getInstance();
         contacts= database.getReference("Trip");
         database.getReference("Trip").keepSynced(true);
+        a_list= new ArrayList<>();
         setContentView(R.layout.get_list);
         ListView list = findViewById(R.id.show_list);
         final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, android.R.id.text1);
@@ -38,7 +47,9 @@ public class getList extends AppCompatActivity {
             @Override
             public void onChildAdded(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 if(dataSnapshot.child("From").getValue()!=null && dataSnapshot.child("Dst").getValue()!=null) {
-                    adapter.add((String) dataSnapshot.child("From").getValue() + "->" + (String) dataSnapshot.child("Dst").getValue());
+                    String key = dataSnapshot.getKey().toString();
+                    a_list.add(key);
+                    adapter.add((String) dataSnapshot.child("From").getValue() + "->" + (String) dataSnapshot.child("Dst").getValue()+"  Driver Name: "+(String) dataSnapshot.child("Driver").getValue()+"   "+(String) dataSnapshot.child("Date").getValue());
                     adapter.notifyDataSetChanged();
                 }
 
@@ -46,7 +57,7 @@ public class getList extends AppCompatActivity {
             @Override
             public void onChildChanged(@NonNull DataSnapshot dataSnapshot, @Nullable String s) {
                 if(dataSnapshot.child("From").getValue()!=null && dataSnapshot.child("Dst").getValue()!=null && i==2){
-                    adapter.add((String)dataSnapshot.child("From").getValue()+ "->"+ (String)dataSnapshot.child("Dst").getValue());
+                    adapter.add((String) dataSnapshot.child("From").getValue() + "->" + (String) dataSnapshot.child("Dst").getValue()+"  Driver Name: "+(String) dataSnapshot.child("Driver").getValue()+"   "+(String) dataSnapshot.child("Date").getValue());
                     i=0;
                 }
                 adapter.notifyDataSetChanged();
@@ -55,7 +66,7 @@ public class getList extends AppCompatActivity {
 
             @Override
             public void onChildRemoved(@NonNull DataSnapshot dataSnapshot) {
-                adapter.remove((String) dataSnapshot.child("From").getValue()+ "->" +(String) dataSnapshot.child("Dst").getValue());
+                adapter.remove((String) dataSnapshot.child("From").getValue() + "->" + (String) dataSnapshot.child("Dst").getValue()+"  Driver Name: "+(String) dataSnapshot.child("Driver").getValue()+"   "+(String) dataSnapshot.child("Date").getValue());
 
             }
 
@@ -79,10 +90,22 @@ public class getList extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        list.setOnItemClickListener( new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                System.out.println(a_list.get(i));
+                contacts.child(a_list.get(i)).removeValue();
+                for( int j =i; j +1< a_list.size();j++){
+                    a_list.set(j,a_list.get(j+1));
+                }
+                new AlertDialog.Builder(getList.this).setTitle("Confirm?").setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int  i ) {
+                    }
+                }).setNegativeButton("cancle", null).create().show();
+            }
+        });
     }
-//    public void onDestroy() {
-//        super.onDestroy();
-//        contacts.removeEventListener((ChildEventListener) this);
-//    }
 
 }
